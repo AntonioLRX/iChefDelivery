5098f0b9642d3fc779417d24e443d2c1255dcf90
Antonio-Xavier <lucas.antonio1232@gmail.com>
2025-11-10 21:59:35 -0300

    Initial Commit

---
+++ iChefDelivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iChefDelivery
+//
+//  Created by Antonio Lucas Reginaldo Xavier on 10/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iChefDelivery/iChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChefDeliveryApp.swift
+//  iChefDelivery
+//
+//  Created by Antonio Lucas Reginaldo Xavier on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct iChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
