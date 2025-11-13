7ad0bb11ee542b1699f19ed24ee68aed7622e058
Antonio-Xavier <lucas.antonio1232@gmail.com>
2025-11-12 23:31:23 -0300

    Add OrderType model struct
    Introduces the OrderType struct conforming to Identifiable, with id, name, and image properties for use in the iChefDelivery app.

---
+++ iChefDelivery/Model/OrderType.swift
@@ -0,0 +1,14 @@
+//
+//  OrderType.swift
+//  iChefDelivery
+//
+//  Created by Antonio Lucas Reginaldo Xavier on 12/11/25.
+//
+
+import Foundation
+
+struct OrderType: Identifiable {
+    let id: Int
+    let name: String
+    let image: String
+}
