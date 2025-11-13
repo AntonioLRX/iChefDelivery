30dfabd29406f3f6e293ee33bdb3ef8a7dab9a85
Antonio-Xavier <lucas.antonio1232@gmail.com>
2025-11-12 23:32:04 -0300

    Refactor ContentView layout and add NavigationBar
    Replaces the initial static content in ContentView with a NavigationBar and a ScrollView containing OrderTypeGridView. Cleans up preview definitions for improved clarity.

--- iChefDelivery/App/ContentView.swift
+++ iChefDelivery/App/ContentView.swift
@@ -10,26 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "star")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("My first class!")
-            Button {
-                print("clicado")
-            } label: {
-                Text("Clica em mim🫦".uppercased()).font(.caption2).padding(.all)
-                
-            }.background(.white).cornerRadius(CGFloat(6)).buttonBorderShape(.circle).border(Color.gray)
-
+            NavigationBar()
+                .padding(.horizontal, 15)
+            ScrollView(
+                .vertical, showsIndicators: false
+            ) {
+                VStack {
+                    OrderTypeGridView()
+                }
+            }
         }
         .padding()
     }
 }
 
-#Preview {
+#Preview(traits: .sizeThatFitsLayout) {
     ContentView()
 }
-
-#Preview(traits: .sizeThatFitsLayout, body: {
-    ContentView()
-})
