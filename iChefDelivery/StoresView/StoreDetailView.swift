6d7081a581cc95fe669776bb7180b33d0a54bd59
Antonio-Xavier <lucas.antonio1232@gmail.com>
2025-11-16 20:33:21 -0300

    Enhance StoreDetailView with images and layout
    Updated StoreDetailView to display store header image, logo, location, and star rating. Improved layout using VStack and HStack, and added section for products.

--- iChefDelivery/StoresView/StoreDetailView.swift
+++ iChefDelivery/StoresView/StoreDetailView.swift
@@ -10,9 +10,47 @@ import SwiftUI
 struct StoreDetailView: View {
     
     let store: StoreType
+    var contador: Int = 0
     
     var body: some View {
-        Text(store.name)
+        VStack(alignment: .leading) {
+            Image(store.headerImage)
+                .resizable()
+                .scaledToFit()
+            HStack {
+                Text(store.name)
+                    .font(.title)
+                    .bold()
+                Spacer()
+                Image(store.logoImage)
+                
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal)
+            
+            HStack {
+                Text(store.location)
+                
+                Spacer()
+                
+                ForEach(1...5, id: \.self) { index in
+                    Image(systemName: index <= store.stars ? "star.fill" : "star")
+                        .foregroundColor(index <= store.stars ? .yellow : .gray)
+                        .font(.caption)
+                    
+                }
+                
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal)
+            
+            Text("Produtos")
+                .font(.title2)
+                .bold()
+                .padding()
+        }
+        .navigationTitle(store.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
