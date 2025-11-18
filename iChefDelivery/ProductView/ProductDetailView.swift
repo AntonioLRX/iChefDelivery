e294cd48a7154931977bf1c82387ea78bd31e5c0
Antonio-Xavier <lucas.antonio1232@gmail.com>
2025-11-18 20:30:23 -0300

    Redesign ProductDetailView layout and add cart button
    Refactored ProductDetailView to improve layout with better spacing and alignment. Added a prominent 'Add to Cart' button with styling and shadow effects at the bottom of the view.

--- iChefDelivery/ProductView/ProductDetailView.swift
+++ iChefDelivery/ProductView/ProductDetailView.swift
@@ -12,25 +12,47 @@ struct ProductDetailView: View {
     let product: ProductType
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 8) {
+        VStack {
+            VStack(alignment: .leading, spacing: 16) {
                 Image(product.image)
                     .resizable()
                     .scaledToFit()
+                    .shadow(radius: 20)
                 
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(product.name)
-                        .font(.title)
-                        .bold()
-                    Text(product.description)
-                        
-                    Text(product.formattedPrice)
-                        .font(.title3)
-                        .bold()
-                    
-                }.padding(.vertical, 20)
-                    .padding(.horizontal, 10)
+                Text(product.name)
+                    .font(.title)
+                    .bold()
+                    .padding(.horizontal)
+                
+                Text(product.description)
+                    .padding(.horizontal)
+                
+                Text(product.formattedPrice)
+                    .font(.title3)
+                    .bold()
+                    .padding(.horizontal)
             }
+            Spacer()
+            
+            Button(
+                action: {
+                    print("O botão foi pressionado!")
+                },
+                label: {
+                    HStack {
+                        Image(systemName: "cart")
+                        
+                        Text("Adicionar ao carrinho")
+                    }
+                    .padding(.horizontal, 32)
+                    .padding(.vertical, 16)
+                    .font(.title3)
+                    .bold()
+                    .background(Color("ColorRed"))
+                    .foregroundColor(.white)
+                    .cornerRadius(32)
+                    .shadow(color: Color("ColorRedDark").opacity(0.7), radius: 10, x: 6, y:8)
+                })
         }
     }
 }
