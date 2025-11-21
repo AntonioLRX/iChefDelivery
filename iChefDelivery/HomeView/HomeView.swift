e2b3b419b4b89f09a5cd9602e4fe7b566d7df7dd
Antonio-Xavier <lucas.antonio1232@gmail.com>
2025-11-21 13:41:09 -0300

    Enhance HomeView animation transitions
    Updated the HomeView to animate circle positions, sizes, and text properties based on the isAnimating state. Increased animation duration for smoother transitions and improved initial layout for a more dynamic entrance effect.

--- iChefDelivery/HomeView/HomeView.swift
+++ iChefDelivery/HomeView/HomeView.swift
@@ -17,25 +17,32 @@ struct HomeView: View {
             ZStack {
                 Circle()
                     .foregroundColor(Color("ColorRed"))
-                    .frame(width: 200)
-                    .position(x: 50, y: 100)
+                    .frame(width: isAnimating ? 200 : 0)
+                    .position(
+                        x: isAnimating ? 50 : -50,
+                        y: isAnimating ? 100 : -100,
+                    )
                     .blur(radius: 60)
                     .opacity(isAnimating ? 0.5 : 0)
                 
                 Circle()
                     .foregroundColor(Color("ColorRedDark"))
-                    .frame(width: 200)
-                    .position(x: geometry.size.width - 50, y: geometry.size.height - 50)
+                    .frame(width: isAnimating ? 200 : 0)
+                    .position(
+                        x: isAnimating ? geometry.size.width - 50 : geometry.size.width + 50,
+                        y: isAnimating ? geometry.size.height - 50 : geometry.size.height + 50,
+                    )
                     .blur(radius: 60)
                     .opacity(isAnimating ? 0.5 : 0)
                 
                 VStack {
                     Text("iChef Delivery!")
-                        .font(.system(size: 40))
+                        .font(.system(size: isAnimating ? 40 : 20))
                         .fontWeight(.heavy)
                         .foregroundColor(Color("ColorRed"))
                         .opacity(isAnimating ? 1 : 0)
                         .offset(y: isAnimating ? 0 : -40)
+                        .frame(width: isAnimating ? geometry.size.width :  30)
                     
                     Text("Peça as suas comidas no conforto da sua casa")
                         .font(.title2)
@@ -44,12 +51,13 @@ struct HomeView: View {
                         .foregroundColor(.black.opacity(0.7))
                         .opacity(isAnimating ? 1 : 0)
                         .offset(y: isAnimating ? 0 : -40)
+                        .frame(width: isAnimating ? geometry.size.width : 30)
                     
                     Spacer()
                 }
                 .onAppear
                 {
-                    withAnimation(.easeInOut(duration: 1.5)) {
+                    withAnimation(.easeInOut(duration: 4)) {
                         isAnimating = true
                     }
                     
