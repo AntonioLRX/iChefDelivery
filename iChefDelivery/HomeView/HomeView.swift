aee1a068940d7d406f92bce3214c0de5622dd891
Antonio-Xavier <lucas.antonio1232@gmail.com>
2025-11-21 13:23:42 -0300

    Add animated background circles to HomeView
    Introduces two blurred, animated circles as background elements in HomeView using a ZStack and GeometryReader. Enhances visual appeal while retaining existing text and animation logic.

--- iChefDelivery/HomeView/HomeView.swift
+++ iChefDelivery/HomeView/HomeView.swift
@@ -12,30 +12,49 @@ struct HomeView: View {
     @State private var isAnimating: Bool = false
     
     var body: some View {
-        VStack {
-            Text("iChef Delivery!")
-                .font(.system(size: 40))
-                .fontWeight(.heavy)
-                .foregroundColor(Color("ColorRed"))
-                .opacity(isAnimating ? 1 : 0)
-                .offset(y: isAnimating ? 0 : -40)
-            
-            Text("Peça as suas comidas no conforto da sua casa")
-                .font(.title2)
-                .padding()
-                .multilineTextAlignment(.center)
-                .foregroundColor(.black.opacity(0.7))
-                .opacity(isAnimating ? 1 : 0)
-                .offset(y: isAnimating ? 0 : -40)
-            
-            Spacer()
-        }
-        .onAppear
-        {
-            withAnimation(.easeInOut(duration: 1.5)) {
-                isAnimating = true
+        
+        GeometryReader { geometry in
+            ZStack {
+                Circle()
+                    .foregroundColor(Color("ColorRed"))
+                    .frame(width: 200)
+                    .position(x: 50, y: 100)
+                    .blur(radius: 60)
+                    .opacity(isAnimating ? 0.5 : 0)
+                
+                Circle()
+                    .foregroundColor(Color("ColorRedDark"))
+                    .frame(width: 200)
+                    .position(x: geometry.size.width - 50, y: geometry.size.height - 50)
+                    .blur(radius: 60)
+                    .opacity(isAnimating ? 0.5 : 0)
+                
+                VStack {
+                    Text("iChef Delivery!")
+                        .font(.system(size: 40))
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color("ColorRed"))
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(y: isAnimating ? 0 : -40)
+                    
+                    Text("Peça as suas comidas no conforto da sua casa")
+                        .font(.title2)
+                        .padding()
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.black.opacity(0.7))
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(y: isAnimating ? 0 : -40)
+                    
+                    Spacer()
+                }
+                .onAppear
+                {
+                    withAnimation(.easeInOut(duration: 1.5)) {
+                        isAnimating = true
+                    }
+                    
+                }
             }
-            
         }
     }
 }
