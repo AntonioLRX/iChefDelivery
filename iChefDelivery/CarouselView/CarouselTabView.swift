59a742f419c808b2afc0b04c61a53b92135f073a
Antonio-Xavier <lucas.antonio1232@gmail.com>
2025-11-23 00:34:06 -0300

    Add auto-scroll to CarouselTabView
    Introduces a timer to automatically advance the carousel every 3 seconds with animation. The TabView now uses selection binding to track and update the current index, improving user experience.

--- iChefDelivery/CarouselView/CarouselTabView.swift
+++ iChefDelivery/CarouselView/CarouselTabView.swift
@@ -10,18 +10,34 @@ import SwiftUI
 struct CarouselTabView: View {
     
     let orders = ordersCarouselMock
+    @State private var currentIndex = 1
     
     var body: some View {
-        TabView {
+        TabView(selection: $currentIndex) {
             ForEach(orders) { item in
                 CarouselItemView(order: item)
+                    .tag(item.id)
             }
         }
         .frame(height: 180)
         .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear {
+            Timer.scheduledTimer(
+                withTimeInterval: 3,
+                repeats: true
+            ) { _ in
+                withAnimation(.easeInOut(duration: 1)) {
+                    if(currentIndex > orders.count) {
+                        currentIndex = 1
+                    }
+                    currentIndex += 1
+                }
+            }
+        }
     }
 }
 
-#Preview {
+#Preview(traits: .sizeThatFitsLayout) {
     CarouselTabView()
+        .padding()
 }
